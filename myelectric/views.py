61647a7c0270833d22357ed0aaeee909e571f7b8
Gordon Hampton <snodrog61@gmail.com>
2025-04-05 12:22:32 -0600

    first django commit

---
+++ myelectric/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'myelectric/home.html')
+
+def about(request):
+    return render(request, 'myelectric/about.html')
